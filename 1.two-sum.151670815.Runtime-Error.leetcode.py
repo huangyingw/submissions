a4d98a25a86a17c0681440d663ceaff0f2c453dc
Ying Huang <yhuang@imvu.com>
2018-04-26 22:46:35 -0700

    n

---
+++ 1.two-sum.151670815.Runtime-Error.leetcode.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        map = {}
+        result = []
+        for idx, val in enumerate(nums):
+            if val in map.keys():
+                result[0] = map[val]
+                result[1] = idx
+                break
+            else:
+                map[target - val] = idx
+        return result
---
+++ 1.two-sum.151756444.Accepted.leetcode.java
@@ -0,0 +1,23 @@
+public class Solution
+{
+    public int[] twoSum(final int[] numbers, int target)
+    {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int[] result = new int[2];
+
+        for (int i = 0; i < numbers.length; i++)
+        {
+            if (map.containsKey(target - numbers[i]))
+            {
+                result[0] = map.get(target - numbers[i]);
+                result[1] = i;
+                return result;
+            }
+
+            map.put(numbers[i], i);
+        }
+
+        return result;
+    }
+}
+
