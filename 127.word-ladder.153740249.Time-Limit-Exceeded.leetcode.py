e8bad3e9c00addb9721c0c21fb9424fb1aa764ac
Ying Huang <yhuang@imvu.com>
2018-05-11 10:16:53 -0700

    n

---
+++ 127.word-ladder.153740249.Time-Limit-Exceeded.leetcode.py
@@ -0,0 +1,95 @@
+#
+# [127] Word Ladder
+#
+# https://leetcode.com/problems/word-ladder/description/
+#
+# algorithms
+# Medium (20.11%)
+# Total Accepted:    163.1K
+# Total Submissions: 810.9K
+# Testcase Example:  '"hit"\n"cog"\n["hot","dot","dog","lot","log","cog"]'
+#
+# Given two words (beginWord and endWord), and a dictionary's word list, find
+# the length of shortest transformation sequence from beginWord to endWord,
+# such that:
+#
+#
+# Only one letter can be changed at a time.
+# Each transformed word must exist in the word list. Note that beginWord is not
+# a transformed word.
+#
+#
+# Note:
+#
+#
+# Return 0 if there is no such transformation sequence.
+# All words have the same length.
+# All words contain only lowercase alphabetic characters.
+# You may assume no duplicates in the word list.
+# You may assume beginWord and endWord are non-empty and are not the same.
+#
+#
+# Example 1:
+#
+#
+# Input:
+# beginWord = "hit",
+# endWord = "cog",
+# wordList = ["hot","dot","dog","lot","log","cog"]
+#
+# Output: 5
+#
+# Explanation: As one shortest transformation is "hit" -> "hot" -> "dot" ->
+# "dog" -> "cog",
+# return its length 5.
+#
+#
+# Example 2:
+#
+#
+# Input:
+# beginWord = "hit"
+# endWord = "cog"
+# wordList = ["hot","dot","dog","lot","log"]
+#
+# Output: 0
+#
+# Explanation: The endWord "cog" is not in wordList, therefore no possible
+# transformation.
+#
+#
+#
+#
+#
+#
+
+
+class Solution(object):
+    def ladderLength(self, beginWord, endWord, wordList):
+        """
+        :type beginWord: str
+        :type endWord: str
+        :type wordList: List[str]
+        :rtype: int
+        """
+        queue = [beginWord]
+        depth = 0
+
+        while queue:
+            depth += 1
+            size = len(queue)
+            for i in range(size):
+                word = queue.pop(0)
+
+                if word == endWord:
+                    return depth
+
+                for i in range(len(beginWord)):
+                    alphabets = string.ascii_lowercase
+                    for c in alphabets:
+                        newWord = word[:i] + c + word[i + 1:]
+
+                        if newWord in wordList:
+                            queue.append(newWord)
+                            wordList.remove(newWord)
+        return 0
