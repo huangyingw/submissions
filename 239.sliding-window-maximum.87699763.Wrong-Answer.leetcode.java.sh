d57774bd1ab74d14fda1bb1909067451000bfb64
Ying Huang <yhuang@imvu.com>
2018-05-19 22:58:38 -0700

    n

---
+++ 239.sliding-window-maximum.87699763.Wrong-Answer.leetcode.java.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -
+SCRIPT=$(realpath "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+cd "$SCRIPTPATH"
+
+./build.sh
+leetcode user -l
+leetcode test \
+    239.sliding-window-maximum.87699763.Wrong-Answer.leetcode.java \
+    -t '[1,-1]\n1'
