f5ed5ae9c81955d0f4b89985147126ded0c4182b
Ying Huang <yhuang@imvu.com>
2018-05-19 23:29:16 -0700

    n

---
+++ 139.word-break.155003135.Accepted.leetcode.java
@@ -0,0 +1,25 @@
+public class Solution
+{
+    public boolean wordBreak(String s, List<String> dict)
+    {
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+
+        for (int start = 0; start < s.length(); start++)
+        {
+            if (dp[start])
+            {
+                for (int end = start + 1; end <= s.length(); end++)
+                {
+                    if (dict.contains(s.substring(start, end)))
+                    {
+                        dp[end] = true;
+                    }
+                }
+            }
+        }
+
+        return dp[s.length()];
+    }
+}
+
