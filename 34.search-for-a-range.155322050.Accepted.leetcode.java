79c68e206da504f7d1535f51c6fb5a1e87568652
Ying Huang <yhuang@imvu.com>
2018-05-21 22:52:00 -0700

    n

---
+++ 34.search-for-a-range.155322050.Accepted.leetcode.java
@@ -0,0 +1,71 @@
+public class Solution
+{
+    public int[] searchRange(int[] nums, int target)
+    {
+        int[] result = { -1, -1 };
+
+        if (nums == null || nums.length == 0)
+        {
+            return result;
+        }
+
+        int left = 0, right = nums.length - 1;
+
+        while (left < right)
+        {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] >= target)
+            {
+                right = mid;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+
+        if (nums[left] == target)
+        {
+            result[0] = left;
+        }
+        else
+        {
+            return result;
+        }
+
+        left = 0;
+        right = nums.length - 1;
+
+        while (left + 1 < right)
+        {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] <= target)
+            {
+                left = mid;
+            }
+            else
+            {
+                right = mid;
+            }
+        }
+
+        if (nums[right] == target)
+        {
+            result[1] = right;
+        }
+        else if (nums[left] == target)
+        {
+            result[1] = left;
+        }
+        else
+        {
+            result[0] = result[1] = -1;
+            return result;
+        }
+
+        return result;
+    }
+}
+
