f93d79ecc6d2a98c09f79c1ad2a26671d58794f2
Ying Huang <yhuang@imvu.com>
2018-05-25 22:33:10 -0700

    n

---
+++ 94.binary-tree-inorder-traversal.155890816.Accepted.leetcode.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution
+{
+    public List<Integer> inorderTraversal(TreeNode root)
+    {
+        List<Integer> result = new ArrayList<Integer>();
+
+        if (root == null)
+        {
+            return result;
+        }
+
+        inorderTraversal(root, result);
+        return result;
+    }
+
+    public void inorderTraversal(TreeNode root, List<Integer> result)
+    {
+        if (root == null)
+        {
+            return;
+        }
+
+        inorderTraversal(root.left, result);
+        result.add(root.val);
+        inorderTraversal(root.right, result);
+    }
+}
+
