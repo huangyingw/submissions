83e27f02a300227730c8e6bc6eabf1d13b07e77f
Ying Huang <yhuang@imvu.com>
2018-06-03 09:02:52 -0700

    n

--- 11.container-with-most-water.88763301.Accepted.leetcode.java
+++ 11.container-with-most-water.88763301.Accepted.leetcode.java
@@ -8,19 +8,18 @@ public class Solution
 
         while (start < end)
         {
+            maxArea = Math.max(maxArea, (end - start) * Math.min(height[start], height[end]));
+
             if (height[start] < height[end])
             {
-                maxArea = Math.max(maxArea, (end - start) * height[start]);
-                start++;
+                start++ ;
             }
             else
             {
-                maxArea = Math.max(maxArea, (end - start) * height[end]);
-                end--;
+                end-- ;
             }
         }
 
         return maxArea;
     }
 }
-
