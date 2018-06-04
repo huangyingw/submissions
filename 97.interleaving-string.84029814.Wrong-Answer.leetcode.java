88a7da3834b3c72895f59e76c01141bc84035ec3
Ying Huang <yhuang@imvu.com>
2018-06-03 22:36:10 -0700

    n

--- 97.interleaving-string.84029814.Wrong-Answer.leetcode.java
+++ 97.interleaving-string.84029814.Wrong-Answer.leetcode.java
@@ -1,40 +1,34 @@
 public class Solution
 {
-    public boolean isInterleave(String s1, String s2, String s3)
-    {
-        if (s1.length() + s2.length() != s3.length())
-        {
-            return false;
-        }
+	public boolean isInterleave(String s1, String s2, String s3)
+	{
+		if (s1.length() + s2.length() != s3.length())
+		{
+			return false;
+		}
 
-        boolean [][] dp = new boolean[s1.length() + 1][s2.length() + 1];
-        dp[0][0] = true;
+		boolean [][] dp = new boolean[s1.length() + 1][s2.length() + 1];
+		dp[0][0] = true;
 
-        for (int i = 1; i <= s1.length(); i++)
-        {
-            if (s1.charAt(i - 1) == s3.charAt(i - 1) && dp[i - 1][0])
-            {
-                dp[i][0] = true;
-            }
-        }
+		for (int i = 1; i <= s1.length(); i++)
+		{
+			dp[i][0] = s1.charAt(i - 1) == s3.charAt(i - 1) && dp[i - 1][0];
+		}
 
-        for (int j = 1; j <= s2.length(); j++)
-        {
-            if (s2.charAt(j - 1) == s3.charAt(j - 1) && dp[0][j - 1])
-            {
-                dp[0][j] = true;
-            }
-        }
+		for (int j = 1; j <= s2.length(); j++)
+		{
+			dp[0][j] = s2.charAt(j - 1) == s3.charAt(j - 1) && dp[0][j - 1];
+		}
 
-        for (int i = 0; i <= s1.length(); i++)
-        {
-            for (int j = 0; j <= s2.length(); j++)
-            {
-                dp[i][j] = (i >= 1 && s1.charAt(i - 1) == s3.charAt(i + j - 1) && dp[i - 1][j])
-                           || (j >= 1 && s2.charAt(j - 1) == s3.charAt(i + j - 1) && dp[i][j - 1]);
-            }
-        }
+		for (int i = 0; i <= s1.length(); i++)
+		{
+			for (int j = 0; j <= s2.length(); j++)
+			{
+				dp[i][j] = (i >= 1 && s1.charAt(i - 1) == s3.charAt(i + j - 1) && dp[i - 1][j])
+				           || (j >= 1 && s2.charAt(j - 1) == s3.charAt(i + j - 1) && dp[i][j - 1]);
+			}
+		}
 
-        return dp[s1.length()][s2.length()];
-    }
+		return dp[s1.length()][s2.length()];
+	}
 }
