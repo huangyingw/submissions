16aedb2cde1a5e5ba88edc3ce86fc7efb902ae45
Ying Huang <yhuang@imvu.com>
2018-06-05 21:10:24 -0700

    n

--- 105.construct-binary-tree-from-preorder-and-inorder-traversal.7782953.Accepted.leetcode.java
+++ 105.construct-binary-tree-from-preorder-and-inorder-traversal.7782953.Accepted.leetcode.java
@@ -1,7 +1,6 @@
 public class Solution
 {
-    private TreeNode buildTree(int[] preorder, int b1,
-                               Map<Integer, Integer> inorderNodes, int b2, int len)
+    private TreeNode buildTree(int[] preorder, int b1, Map<Integer, Integer> inorderNodes, int b2, int len)
     {
         if (len == 0)
         {
@@ -11,15 +10,13 @@ public class Solution
         TreeNode node = new TreeNode(preorder[b1]);
         int i = inorderNodes.get(node.val);
         node.left = buildTree(preorder, b1 + 1, inorderNodes, b2, i - b2);
-        node.right = buildTree(preorder, b1 + 1 + i - b2, inorderNodes,
-                               i + 1, len - i + b2 - 1);
+        node.right = buildTree(preorder, b1 + 1 + i - b2, inorderNodes, i + 1, len - i + b2 - 1);
         return node;
     }
 
     public TreeNode buildTree(int[] preorder, int[] inorder)
     {
-        assert(preorder != null && inorder != null
-               && preorder.length == inorder.length);
+        assert(preorder != null && inorder != null && preorder.length == inorder.length);
         Map<Integer, Integer> inorderNodes = new HashMap<Integer, Integer>();
 
         for (int i = 0; i < inorder.length; i++)
@@ -30,4 +27,3 @@ public class Solution
         return buildTree(preorder, 0, inorderNodes, 0, inorder.length);
     }
 }
-
