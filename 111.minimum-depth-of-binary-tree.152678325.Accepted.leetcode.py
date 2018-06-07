3a564603d2f864cc2b30192cf0019fbd4c4b1001
Ying Huang <yhuang@imvu.com>
2018-06-07 15:47:30 -0700

    n

--- 111.minimum-depth-of-binary-tree.152678325.Accepted.leetcode.py
+++ 111.minimum-depth-of-binary-tree.152678325.Accepted.leetcode.py
@@ -1,15 +1,15 @@
 class Solution(object):
     def minDepth(self, root):
-        """
-        :type root: TreeNode
-        :rtype: int
-        """
         if root is None:
             return 0
+
         left = self.minDepth(root.left)
         right = self.minDepth(root.right)
+
         if root.left is None:
             return right + 1
+
         if root.right is None:
             return left + 1
+
         return min(left, right) + 1
