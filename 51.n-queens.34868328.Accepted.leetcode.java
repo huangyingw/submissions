41c3247d9a2c5b7724266d666164677f2166ed89
Ying Huang <yhuang@imvu.com>
2018-06-08 16:33:34 -0700

    n

--- 51.n-queens.34868328.Accepted.leetcode.java
+++ 51.n-queens.34868328.Accepted.leetcode.java
@@ -2,22 +2,22 @@ public class Solution
 {
     public List<List<String>> solveNQueens(int n)
     {
-        List<List<String>> res = new ArrayList<List<String>>();
-        dfs(res, new int[n], 0, n);
-        return res;
+        List<List<String>> result = new ArrayList<List<String>>();
+        dfs(result, new int[n], 0, n);
+        return result;
     }
 
-    public void dfs(List<List<String>> res, int[] usedColumns, int curRow, int total)
+    public void dfs(List<List<String>> result, int[] usedColumns, int curRow, int n)
     {
-        if (curRow == total)
+        if (curRow == n)
         {
             List<String> board = new ArrayList<String>();
 
-            for (int row = 0; row < total; row++)
+            for (int row = 0; row < n; row++)
             {
                 StringBuilder sb = new StringBuilder();
 
-                for (int col = 0; col < total; col++)
+                for (int col = 0; col < n; col++)
                 {
                     if (col == usedColumns[row])
                     {
@@ -32,17 +32,17 @@ public class Solution
                 board.add(sb.toString());
             }
 
-            res.add(board);
+            result.add(board);
             return;
         }
 
-        for (int col = 0; col < total; col++)
+        for (int col = 0; col < n; col++)
         {
             usedColumns[curRow] = col;
 
             if (isValidSolution(curRow, usedColumns))
             {
-                dfs(res, usedColumns, curRow + 1, total);
+                dfs(result, usedColumns, curRow + 1, n);
             }
 
             usedColumns[curRow] = -1;
