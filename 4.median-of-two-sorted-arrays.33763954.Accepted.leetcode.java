423dab6589d0aec8deb54ad356b2ad493eaac2b4
Ying Huang <yhuang@imvu.com>
2018-06-09 12:25:15 -0700

    n

--- 4.median-of-two-sorted-arrays.33763954.Accepted.leetcode.java
+++ 4.median-of-two-sorted-arrays.33763954.Accepted.leetcode.java
@@ -5,19 +5,17 @@ public class Solution
         int m = A.length;
         int n = B.length;
 
-        if ((m + n) % 2 != 0) // odd
+        if ((m + n) % 2 != 0)
         {
             return (double) findKth(A, B, (m + n) / 2, 0, m - 1, 0, n - 1);
         }
-        else   // even
+        else
         {
-            return (findKth(A, B, (m + n) / 2, 0, m - 1, 0, n - 1)
-                    + findKth(A, B, (m + n) / 2 - 1, 0, m - 1, 0, n - 1)) * 0.5;
+            return (findKth(A, B, (m + n) / 2, 0, m - 1, 0, n - 1) + findKth(A, B, (m + n) / 2 - 1, 0, m - 1, 0, n - 1)) * 0.5;
         }
     }
 
-    public int findKth(int A[], int B[], int k,
-                       int aStart, int aEnd, int bStart, int bEnd)
+    public int findKth(int A[], int B[], int k, int aStart, int aEnd, int bStart, int bEnd)
     {
         int aLen = aEnd - aStart + 1;
         int bLen = bEnd - bStart + 1;
@@ -37,8 +35,8 @@ public class Solution
             return A[aStart] < B[bStart] ? A[aStart] : B[bStart];
         }
 
-        int aMid = aLen * k / (aLen + bLen); // a's middle count
-        int bMid = k - aMid - 1; // b's middle count
+        int aMid = aLen * k / (aLen + bLen);
+        int bMid = k - aMid - 1;
         aMid = aMid + aStart;
         bMid = bMid + bStart;
 
@@ -58,4 +56,3 @@ public class Solution
         return findKth(A, B, k, aStart, aEnd, bStart, bEnd);
     }
 }
-
