c6642a77adf54e2527d2d9e578689a3921b2a2a7
Ying Huang <yhuang@imvu.com>
2018-06-09 21:35:15 -0700

    n

--- 72.edit-distance.97357633.Accepted.leetcode.java
+++ 72.edit-distance.97357633.Accepted.leetcode.java
@@ -18,12 +18,10 @@ public class Solution
                 if (word1.charAt(i - 1) == word2.charAt(j - 1))
                 {
                     dp[i][j] = dp[i - 1][j - 1];
-                    //System.out.println(i + "," + j + " --> " + dp[i][j]);
                 }
                 else
                 {
                     dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i - 1][j - 1]), dp[i][j - 1]) + 1;
-                    //System.out.println(i + "," + j + " --> " + dp[i][j]);
                 }
             }
         }
