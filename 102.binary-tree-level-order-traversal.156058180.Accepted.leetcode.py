15bcc22a005c7567b1bdd03892eb78474046c3bd
Ying Huang <yhuang@imvu.com>
2018-06-11 23:19:32 -0700

    n

--- 102.binary-tree-level-order-traversal.156058180.Accepted.leetcode.py
+++ 102.binary-tree-level-order-traversal.156058180.Accepted.leetcode.py
@@ -1,22 +1,25 @@
 class Solution(object):
     def levelOrder(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[List[int]]
-        """
         if not root:
             return []
+
         queue = [root]
         result = []
+
         while queue:
             current = []
             size = len(queue)
+
             for _ in range(size):
                 root = queue.pop(0)
                 current.append(root.val)
+
                 if root.left:
                     queue.append(root.left)
+
                 if root.right:
                     queue.append(root.right)
+
             result.append(current)
+
         return result
