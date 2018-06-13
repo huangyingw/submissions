5bc5255a0561d7664384de88ab88b8295c212194
Ying Huang <yhuang@imvu.com>
2018-06-12 23:28:49 -0700

    n

--- 108.convert-sorted-array-to-binary-search-tree.153145598.Accepted.leetcode.py
+++ 108.convert-sorted-array-to-binary-search-tree.153145598.Accepted.leetcode.py
@@ -1,7 +1,7 @@
 class Solution(object):
     def sortedArrayToBST(self, nums):
         def sortedArrayToBST(nums, left, right):
-            if left > right or left < 0 or right < 0:
+            if left > right:
                 return None
 
             mid = (left + right) / 2
@@ -9,5 +9,4 @@ class Solution(object):
             root.left = sortedArrayToBST(nums, left, mid - 1)
             root.right = sortedArrayToBST(nums, mid + 1, right)
             return root
-
         return sortedArrayToBST(nums, 0, len(nums) - 1)
