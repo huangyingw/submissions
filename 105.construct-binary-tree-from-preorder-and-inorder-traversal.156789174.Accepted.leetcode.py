fc2d56b24baaccd3566a97101a186e98cb365831
Ying Huang <yhuang@imvu.com>
2018-06-12 21:08:36 -0700

    n

--- 105.construct-binary-tree-from-preorder-and-inorder-traversal.156789174.Accepted.leetcode.py
+++ 105.construct-binary-tree-from-preorder-and-inorder-traversal.156789174.Accepted.leetcode.py
@@ -4,16 +4,16 @@ class Solution(object):
         for idx, val in enumerate(inorder):
             map[val] = idx
 
-        def helper(preLeft, preRight, inLeft, inRight):
+        def dfs(preLeft, preRight, inLeft, inRight):
             if preLeft > preRight:
                 return None
 
             root = TreeNode(preorder[preLeft])
             idx = map[preorder[preLeft]]
-            root.left = helper(preLeft + 1, preLeft +
-                               idx - inLeft, inLeft, idx - 1)
-            root.right = helper(preRight - inRight + idx +
-                                1, preRight, idx + 1, inRight)
+            root.left = dfs(preLeft + 1, preLeft +
+                            idx - inLeft, inLeft, idx - 1)
+            root.right = dfs(preRight - inRight + idx +
+                             1, preRight, idx + 1, inRight)
             return root
 
-        return helper(0, len(preorder) - 1, 0, len(inorder) - 1)
+        return dfs(0, len(preorder) - 1, 0, len(inorder) - 1)
