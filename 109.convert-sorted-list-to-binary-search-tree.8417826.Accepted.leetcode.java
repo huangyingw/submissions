0c1713fa861a097031fc72d9125a82b921ced8e7
Ying Huang <yhuang@imvu.com>
2018-06-13 15:51:59 -0700

    n

--- 109.convert-sorted-list-to-binary-search-tree.8417826.Accepted.leetcode.java
+++ 109.convert-sorted-list-to-binary-search-tree.8417826.Accepted.leetcode.java
@@ -1,34 +1,27 @@
 public class Solution
 {
-    ListNode h;
+    private ListNode current;
+
+    private int getListLength(ListNode head)
+    {
+        int size = 0;
+
+        while (head != null)
+        {
+            size++;
+            head = head.next;
+        }
+
+        return size;
+    }
 
     public TreeNode sortedListToBST(ListNode head)
     {
-        if (head == null)
-        {
-            return null;
-        }
-
-        h = head;
-        int len = getLength(head);
-        return sortedListToBST(0, len - 1);
+        current = head;
+        return sortedListToBSTHelper(0, getListLength(head) - 1);
     }
 
-    public int getLength(ListNode head)
-    {
-        int len = 0;
-        ListNode p = head;
-
-        while (p != null)
-        {
-            len++;
-            p = p.next;
-        }
-
-        return len;
-    }
-
-    public TreeNode sortedListToBST(int start, int end)
+    public TreeNode sortedListToBSTHelper(int start, int end)
     {
         if (start > end)
         {
@@ -36,13 +29,12 @@ public class Solution
         }
 
         int mid = (start + end) / 2;
-        TreeNode left = sortedListToBST(start, mid - 1);
-        TreeNode root = new TreeNode(h.val);
-        h = h.next;
-        TreeNode right = sortedListToBST(mid + 1, end);
+        TreeNode left = sortedListToBSTHelper(start, mid - 1);
+        TreeNode root = new TreeNode(current.val);
+        current = current.next;
+        TreeNode right = sortedListToBSTHelper(mid + 1, end);
         root.left = left;
         root.right = right;
         return root;
     }
 }
-
