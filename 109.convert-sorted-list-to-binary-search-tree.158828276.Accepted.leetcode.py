9937d3d48c745448008dc43ebe0c6355e82348b0
Ying Huang <yhuang@imvu.com>
2018-06-13 23:36:35 -0700

    n

--- 109.convert-sorted-list-to-binary-search-tree.158828276.Accepted.leetcode.py
+++ 109.convert-sorted-list-to-binary-search-tree.158828276.Accepted.leetcode.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    def sortedArrayToBST(self, nums):
+    def sortedListToBST(self, head):
         def sortedArrayToBST(nums, left, right):
             if left > right:
                 return None
@@ -9,4 +9,10 @@ class Solution(object):
             root.left = sortedArrayToBST(nums, left, mid - 1)
             root.right = sortedArrayToBST(nums, mid + 1, right)
             return root
+        nums = []
+
+        while head:
+            nums.append(head.val)
+            head = head.next
+
         return sortedArrayToBST(nums, 0, len(nums) - 1)
