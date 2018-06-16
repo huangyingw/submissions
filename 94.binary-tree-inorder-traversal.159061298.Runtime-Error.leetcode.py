a5d9208d699bc23973e5b0ca0969c9456e939249
Ying Huang <yhuang@imvu.com>
2018-06-15 17:20:49 -0700

    n

---
+++ 94.binary-tree-inorder-traversal.159061298.Runtime-Error.leetcode.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def inorderTraversal(self, root):
+        result = []
+        stack = []
+
+        while stack or root:
+            if root.left:
+                stack.append(root)
+                root = root.left
+            else:
+                result.append(root.val)
+                root = stack.pop()
+                root = root.right
+        return result
