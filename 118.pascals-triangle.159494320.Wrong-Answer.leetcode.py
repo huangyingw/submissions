4b2e95ac807a6d70979e79b20fee5aae3ea7fb29
Ying Huang <yhuang@imvu.com>
2018-06-18 20:49:08 -0700

    n

---
+++ 118.pascals-triangle.159494320.Wrong-Answer.leetcode.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def generate(self, numRows):
+        result = []
+
+        for i in range(numRows):
+            result.append([1] * i)
+
+        return result
---
+++ 118.pascals-triangle.159494611.Wrong-Answer.leetcode.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def generate(self, numRows):
+        result = []
+
+        for i in range(1, numRows + 1):
+            result.append([1] * i)
+
+        return result
