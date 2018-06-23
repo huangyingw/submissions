bd54289dd5673d8b9d83816470bbe5e92101633d
Ying Huang <yhuang@imvu.com>
2018-06-23 15:16:08 -0700

    n

--- 124.binary-tree-maximum-path-sum.73741101.Accepted.leetcode.java
+++ 124.binary-tree-maximum-path-sum.73741101.Accepted.leetcode.java
@@ -1,6 +1,12 @@
 public class Solution
 {
-    private int max;
+    int max;
+    public int maxPathSum(TreeNode root)
+    {
+        max = Integer.MIN_VALUE;
+        findMax(root);
+        return max;
+    }
     public int findMax(TreeNode root)
     {
         if (root == null)
@@ -12,11 +18,5 @@ public class Solution
         int right = Math.max(findMax(root.right), 0);
         max = Math.max(max, root.val + left + right);
         return root.val + Math.max(left, right);
-    }
-    public int maxPathSum(TreeNode root)
-    {
-        max = Integer.MIN_VALUE;
-        findMax(root);
-        return max;
     }
 }
