20ec59d3d4a5c91a4dc053717008b825628c9403
Ying Huang <yhuang@imvu.com>
2018-06-30 17:23:28 -0700

    n

---
+++ 21.merge-two-sorted-lists.161358342.Time-Limit-Exceeded.leetcode.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        dummy = ListNode(-1)
+        nav = dummy
+
+        while l1 or l2:
+            if not l2 or (l1 and l2.val >= l1.val):
+                nav.next = l1
+            else:
+                nav.next = l2
+            nav = nav.next
+
+        return dummy.next
