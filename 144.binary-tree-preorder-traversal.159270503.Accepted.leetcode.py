8d320754d50da1635f3147c566f320271e04eeb3
Ying Huang <yhuang@imvu.com>
2018-07-01 00:24:58 -0700

    n

--- 144.binary-tree-preorder-traversal.159270503.Accepted.leetcode.py
+++ 144.binary-tree-preorder-traversal.159270503.Accepted.leetcode.py
@@ -5,8 +5,10 @@ class Solution(object):
         def dfs(root):
             if not root:
                 return
+
             result.append(root.val)
             dfs(root.left)
             dfs(root.right)
+
         dfs(root)
         return result
