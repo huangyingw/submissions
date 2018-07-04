e09c17e569f1e3237ca5eb7279e36d2ab677e49a
Ying Huang <yhuang@imvu.com>
2018-07-03 20:18:33 -0700

    n

--- 149.max-points-on-a-line.96709241.Wrong-Answer.leetcode.java
+++ 149.max-points-on-a-line.96709241.Wrong-Answer.leetcode.java
@@ -1,12 +1,3 @@
-/**
- * Definition for a point.
- * class Point {
- *     int x;
- *     int y;
- *     Point() { x = 0; y = 0; }
- *     Point(int a, int b) { x = a; y = b; }
- * }
- */
 public class Solution
 {
     public int maxPoints(Point[] points)
