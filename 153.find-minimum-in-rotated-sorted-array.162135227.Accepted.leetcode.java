9456e7c09a60010cdb659089c855d2b6f02edea1
Ying Huang <yhuang@imvu.com>
2018-07-05 00:09:38 -0700

    n

---
+++ 153.find-minimum-in-rotated-sorted-array.162135227.Accepted.leetcode.java
@@ -0,0 +1,35 @@
+public class Solution
+{
+    public int findMin(int[] nums)
+    {
+        if (nums == null || nums.length == 0)
+        {
+            return 0;
+        }
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left + 1 < right)
+        {
+            if (nums[left] < nums[right])
+            {
+                return nums[left];
+            }
+
+            int mid = (left + right) / 2;
+
+            if (nums[mid] < nums[left])
+            {
+                right = mid;
+            }
+            else
+            {
+                left = mid;
+            }
+        }
+
+        return Math.min(nums[left], nums[right]);
+    }
+}
+
