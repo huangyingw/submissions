f2f29eb6fd8e7d22c9e8362221784859c9dc8361
Ying Huang <yhuang@imvu.com>
2018-07-07 16:32:27 -0700

    n

---
+++ 157.read-n-characters-given-read4.162609647.Time-Limit-Exceeded.leetcode.java
@@ -0,0 +1,23 @@
+public class Solution extends Reader4
+{
+    public int read(char[] buf, int n)
+    {
+        System.out.printf("n --> %s\n", n);
+        char[] buffer = new char[4];
+        int readBytes = 0;
+        boolean eof = false;
+
+        while (!eof && readBytes < n)
+        {
+            int sz = read4(buffer);
+            int bytes = Math.min(n - readBytes, sz);
+            System.out.printf("bytes --> %s\n", bytes);
+            System.arraycopy(buffer, 0, buf, readBytes, bytes);
+            readBytes += bytes;
+            System.out.printf("readBytes --> %s\n", readBytes);
+        }
+
+        return readBytes;
+    }
+}
+
