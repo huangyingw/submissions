5ce35d4c3e7ec67f09194686123e3c051e34549d
Ying Huang <yhuang@imvu.com>
2018-07-13 10:28:18 -0700

    n

---
+++ 171.excel-sheet-column-number.163569557.Accepted.leetcode.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def titleToNumber(self, s):
+        result = 0
+
+        for char in s:
+            result = result * 26 + ord(char) - ord('A') + 1
+        return result
