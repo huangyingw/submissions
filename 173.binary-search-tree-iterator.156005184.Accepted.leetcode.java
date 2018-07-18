bc77a071431dd3077a70614b3c34bd8a9e9f8f91
Ying Huang <yhuang@imvu.com>
2018-07-17 21:14:38 -0700

    n

--- 173.binary-search-tree-iterator.156005184.Accepted.leetcode.java
+++ 173.binary-search-tree-iterator.156005184.Accepted.leetcode.java
@@ -37,4 +37,3 @@ public class BSTIterator
         return result;
     }
 }
-
--- 173.binary-search-tree-iterator.96640596.Accepted.leetcode.java
+++ 173.binary-search-tree-iterator.96640596.Accepted.leetcode.java
@@ -2,14 +2,17 @@ public class BSTIterator
 {
     private Stack<TreeNode> stack = new Stack<>();
     private TreeNode top;
+
     public BSTIterator(TreeNode root)
     {
         top = root;
     }
+
     public boolean hasNext()
     {
         return (!stack.isEmpty() || top != null);
     }
+
     public int next()
     {
         while (top != null)
