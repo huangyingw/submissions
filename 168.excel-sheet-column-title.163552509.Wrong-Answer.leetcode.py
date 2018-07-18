646eeb6952c0eaaa4f55213bfa4c6c0d5d6d4049
Ying Huang <yhuang@imvu.com>
2018-07-17 22:27:06 -0700

    n

--- 168.excel-sheet-column-title.163552509.Wrong-Answer.leetcode.py
+++ 168.excel-sheet-column-title.163552509.Wrong-Answer.leetcode.py
@@ -4,6 +4,5 @@ class Solution(object):
 
         while n > 0:
             result += chr(ord('A') + (n - 1) % 26)
-            print('result --> %s' % result)
             n /= 26
         return result[::-1]
--- 168.excel-sheet-column-title.163552581.Wrong-Answer.leetcode.py
+++ 168.excel-sheet-column-title.163552581.Wrong-Answer.leetcode.py
@@ -4,7 +4,5 @@ class Solution(object):
 
         while n > 0:
             result += chr(ord('A') + (n - 1) % 26)
-            print('result --> %s' % result)
             n /= 26
-            print('n --> %s' % n)
         return result[::-1]
