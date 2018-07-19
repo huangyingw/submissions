d896b12e9dd9c7e0b26ecd9e6a9c31eebbc54bb5
Ying Huang <yhuang@imvu.com>
2018-07-18 22:06:58 -0700

    n

--- 174.dungeon-game.83985495.Accepted.leetcode.java
+++ 174.dungeon-game.83985495.Accepted.leetcode.java
@@ -2,13 +2,21 @@ public class Solution
 {
     public int calculateMinimumHP(int[][] dungeon)
     {
+        if (dungeon.length == 0)
+        {
+            return 0;
+        }
+
         int m = dungeon.length, n = dungeon[0].length;
-        int[] dp = new int[n + 1];
-        Arrays.fill(dp, 0);
+        int[] dp = new int[n];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[n - 1] = 1;
 
         for (int i = m - 1; i >= 0; i--)
         {
-            for (int j = n - 1; j >= 0; j--)
+            dp[n - 1] = Math.max(dp[n - 1] - dungeon[i][n - 1], 1);
+
+            for (int j = n - 2; j >= 0; j--)
             {
                 dp[j] = Math.max(1, Math.min(dp[j], dp[j + 1]) - dungeon[i][j]);
             }
@@ -17,4 +25,3 @@ public class Solution
         return dp[0];
     }
 }
-
