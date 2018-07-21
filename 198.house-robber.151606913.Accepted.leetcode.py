701db037c5409c350dcb4002c4fd1dd9a864c656
Ying Huang <yhuang@imvu.com>
2018-07-21 16:25:48 -0700

    n

--- 198.house-robber.151606913.Accepted.leetcode.py
+++ 198.house-robber.151606913.Accepted.leetcode.py
@@ -1,10 +1,7 @@
 class Solution(object):
     def rob(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
         now = last = 0
+
         for i in nums:
             last, now = now, max(i + last, now)
         return now
