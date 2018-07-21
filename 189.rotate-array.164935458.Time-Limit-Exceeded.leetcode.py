5235d7ee719f883ede493a8446204da1f4573930
Ying Huang <yhuang@imvu.com>
2018-07-21 11:15:43 -0700

    n

---
+++ 189.rotate-array.164935458.Time-Limit-Exceeded.leetcode.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def rotate(self, nums, k):
+        def reverse(left, right):
+            while left < right:
+                temp = nums[left]
+                nums[left] = nums[right]
+                nums[right] = temp
+
+        reverse(0, len(nums) - 1)
+        reverse(0, k - 1)
+        reverse(k, len(nums) - 1)
