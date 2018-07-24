0d9ff9314ac635ee2950e25a3b8cc5486d119b15
Ying Huang <yhuang@imvu.com>
2018-07-24 16:40:10 -0700

    n

--- 206.reverse-linked-list.156972375.Wrong-Answer.leetcode.py
+++ 206.reverse-linked-list.156972375.Wrong-Answer.leetcode.py
@@ -1,13 +1,11 @@
 class Solution(object):
     def reverseList(self, head):
-        """
-        :type head: ListNode
-        :rtype: ListNode
-        """
         pre = None
+
         while pre:
             temp = head.next
             head.next = pre
             head = temp
             pre = head
+
         return pre
