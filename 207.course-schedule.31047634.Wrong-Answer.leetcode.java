6b5546b7c7fd894d63c8fbbed8a8ce02fb82d1fd
Ying Huang <yhuang@imvu.com>
2018-07-28 16:25:43 -0700

    n

--- 207.course-schedule.31047634.Wrong-Answer.leetcode.java
+++ 207.course-schedule.31047634.Wrong-Answer.leetcode.java
@@ -10,6 +10,7 @@ public class Solution
             graph.add(new HashSet<Integer>());
         }
 
+        // fill the adjacency list
         for (int i = 0; i < prerequisites.length; i++)
         {
             graph.get(prerequisites[i][1]).add(prerequisites[i][0]);
