ccc42d755e48bac2955d5fcc9ca9dc7f2bdad107
Ying Huang <yhuang@imvu.com>
2018-07-29 23:28:57 -0700

    n

--- 79.word-search.87146808.Accepted.leetcode.java
+++ 79.word-search.87146808.Accepted.leetcode.java
@@ -7,12 +7,7 @@ public class Solution
             return true;
         }
 
-        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length)
-        {
-            return false;
-        }
-
-        if (visited[i][j] || board[i][j] != word.charAt(begin))
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || visited[i][j] || board[i][j] != word.charAt(begin))
         {
             return false;
         }
