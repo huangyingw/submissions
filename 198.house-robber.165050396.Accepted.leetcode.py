0138d4c8b1dabcc5482c456b13d75caf5a1f5d63
Ying Huang <yhuang@imvu.com>
2018-08-02 20:26:17 -0700

    n

--- 198.house-robber.165050396.Accepted.leetcode.py
+++
@@ -1,7 +0,0 @@
-class Solution(object):
-    def rob(self, nums):
-        now = last = 0
-
-        for i in nums:
-            last, now = now, max(i + last, now)
-        return now
--- 198.house-robber.165111074.Runtime-Error.leetcode.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def rob(self, nums):
-        last = now = 0
-
-        for num in nums:
-            last = now
-            now = max(last + num, now)
-
-        return now
