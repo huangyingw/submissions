577cd5c80cb81a2e9da67586802b52cbd9ab921e
Ying Huang <yhuang@imvu.com>
2018-08-03 22:34:23 -0700

    n

--- 39.combination-sum.81989737.Wrong-Answer.leetcode.java
+++ 39.combination-sum.81989737.Wrong-Answer.leetcode.java
@@ -1,28 +1,28 @@
 public class Solution
 {
-    public List<List<Integer>> combinationSum(int[] datas, int target)
+    public List<List<Integer>> combinationSum(int[] nums, int target)
     {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
-        helper(datas, 0, result, new ArrayList<Integer>(), target);
+        helper(nums, 0, result, new ArrayList<Integer>(), target);
         return result;
     }
 
-    public void helper(int[] datas, int start, List<List<Integer>> result, List<Integer> current, int target)
+    public void helper(int[] nums, int start, List<List<Integer>> result, List<Integer> current, int target)
     {
         if (target == 0)
         {
             result.add(new ArrayList<Integer>(current));
         }
 
-        for (int i = start; i < datas.length; i++)
+        for (int i = start; i < nums.length; i++)
         {
-            if (target < datas[i])
+            if (target < nums[i])
             {
                 return;
             }
 
-            current.add(datas[i]);
-            helper(datas, i, result, current, target - datas[i]);
+            current.add(nums[i]);
+            helper(nums, i, result, current, target - nums[i]);
             current.remove(current.size() - 1);
         }
     }
