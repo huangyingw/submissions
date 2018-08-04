60a8de1057e01ed84b6beed83c494b3be96efe4e
Ying Huang <yhuang@imvu.com>
2018-08-03 22:51:42 -0700

    n

--- 40.combination-sum-ii.102871851.Accepted.leetcode.java
+++ 40.combination-sum-ii.102871851.Accepted.leetcode.java
@@ -1,14 +1,14 @@
 public class Solution
 {
-    public List<List<Integer>> combinationSum2(int[] num, int target)
+    public List<List<Integer>> combinationSum2(int[] nums, int target)
     {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
-        Arrays.sort(num);
-        dfs(num, 0, target, new ArrayList<Integer>(), result);
+        Arrays.sort(nums);
+        dfs(nums, 0, target, new ArrayList<Integer>(), result);
         return result;
     }
 
-    private void dfs(int[] num, int start, int target, ArrayList<Integer> current, List<List<Integer>> result)
+    private void dfs(int[] nums, int start, int target, ArrayList<Integer> current, List<List<Integer>> result)
     {
         if (target == 0)
         {
@@ -16,20 +16,20 @@ public class Solution
             return;
         }
 
-        if (target < 0 || start >= num.length)
+        if (target < 0 || start >= nums.length)
         {
             return;
         }
 
-        for (int index = start; index < num.length; index++)
+        for (int index = start; index < nums.length; index++)
         {
-            if (index - 1 >= start && num[index] == num[index - 1])
+            if (index - 1 >= start && nums[index] == nums[index - 1])
             {
                 continue;
             }
 
-            current.add(num[index]);
-            dfs(num, index + 1, target - num[index], current, result);
+            current.add(nums[index]);
+            dfs(nums, index + 1, target - nums[index], current, result);
             current.remove(current.size() - 1);
         }
     }
