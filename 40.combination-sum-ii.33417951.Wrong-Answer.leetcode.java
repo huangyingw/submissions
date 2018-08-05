eaa58c4cf83d991d9e170aa35ad1484f2a529524
Ying Huang <yhuang@imvu.com>
2018-08-04 17:21:49 -0700

    n

--- 40.combination-sum-ii.33417951.Wrong-Answer.leetcode.java
+++ 40.combination-sum-ii.33417951.Wrong-Answer.leetcode.java
@@ -14,7 +14,8 @@ public class Solution
         return result;
     }
 
-    private void dfs(int[] num, int start, int target, ArrayList<Integer> current, List<List<Integer>> result)
+    private void dfs(int[] num, int start, int target,
+                     ArrayList<Integer> current, List<List<Integer>> result)
     {
         if (target == 0)
         {
