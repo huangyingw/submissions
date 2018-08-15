18cb68aa7038589171b48c10368d17aa403984b3
Ying Huang <yhuang@imvu.com>
2018-08-14 21:24:44 -0700

    n

--- 221.maximal-square.30967687.Accepted.leetcode.java
+++ 221.maximal-square.30967687.Accepted.leetcode.java
@@ -26,10 +26,7 @@ public class Solution
         {
             for (int j = 1; j < matrix[0].length; j++)
             {
-                dp[i][j] =
-                    matrix[i][j] == '1' ? Math.min(Math.min(dp[i - 1][j],
-                                                   dp[i][j - 1]),
-                                                   dp[i - 1][j - 1]) + 1 : 0;
+                dp[i][j] = matrix[i][j] == '1' ? Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1 : 0;
                 maxLen = Math.max(maxLen, dp[i][j]);
             }
         }
