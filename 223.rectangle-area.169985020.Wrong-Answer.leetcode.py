43c0743d711159c50407a2d0d09b8cd68d607582
Ying Huang <yhuang@imvu.com>
2018-08-17 11:31:43 -0700

    n

---
+++ 223.rectangle-area.169985020.Wrong-Answer.leetcode.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def computeArea(self, A, B, C, D, E, F, G, H):
+        left = max(A, E)
+        right = min(C, G)
+        low = max(B, F)
+        hight = min(H, D)
+        area1 = (C - A) * (D - B)
+        area2 = (G - E) * (H - F)
+        return area1 + area2 - (right - left) * (hight - low)
