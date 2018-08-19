f4df2b7630c855a922554dc19be9fabc3921f238
Ying Huang <yhuang@imvu.com>
2018-08-18 22:11:45 -0700

    n

--- 140.word-break-ii.161244700.Accepted.leetcode.java
+++ 140.word-break-ii.161244700.Accepted.leetcode.java
@@ -1,6 +1,6 @@
 public class Solution
 {
-    List<String> res = new ArrayList<String>();
+    List<String> result = new ArrayList<String>();
     boolean[] f = null;
     char[] s;
     List<String> dict = null;
@@ -10,7 +10,7 @@ public class Solution
     {
         if (p == n)
         {
-            res.add(now);
+            result.add(now);
             return true;
         }
 
@@ -55,6 +55,6 @@ public class Solution
         }
 
         dfs(0, "");
-        return res;
+        return result;
     }
 }
