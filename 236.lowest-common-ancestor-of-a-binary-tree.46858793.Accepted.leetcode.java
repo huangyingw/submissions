464cb6f922c64da0e8c12994ed737ff190004e91
Ying Huang <yhuang@imvu.com>
2018-08-25 22:07:24 -0700

    n

--- 236.lowest-common-ancestor-of-a-binary-tree.46858793.Accepted.leetcode.java
+++ 236.lowest-common-ancestor-of-a-binary-tree.46858793.Accepted.leetcode.java
@@ -1,25 +1,35 @@
 public class Solution
 {
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p,
+                                         TreeNode q)
     {
+        // Base case
         if (root == null)
         {
             return null;
         }
 
+        // If either n1 or n2 matches with root's key, report
+        // the presence by returning root (Note that if a key is
+        // ancestor of other, then the ancestor key becomes LCA
         if (root == p || root == q)
         {
             return root;
         }
 
+        // Look for keys in left and right subtrees
         TreeNode left_lca = lowestCommonAncestor(root.left, p, q);
         TreeNode right_lca = lowestCommonAncestor(root.right, p, q);
 
+        // If both of the above calls return Non-null, then one key
+        // is present in once subtree and other is present in other,
+        // So this node is the LCA
         if (left_lca != null && right_lca != null)
         {
             return root;
         }
 
+        // Otherwise check if left subtree or right subtree is LCA
         return (left_lca != null) ? left_lca : right_lca;
     }
 }
