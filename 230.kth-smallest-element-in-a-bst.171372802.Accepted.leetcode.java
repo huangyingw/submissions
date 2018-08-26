9338e3d0e4de2bcd2d48c9793398533572ec147f
Ying Huang <yhuang@imvu.com>
2018-08-25 21:52:56 -0700

    n

--- 230.kth-smallest-element-in-a-bst.171372802.Accepted.leetcode.java
+++ 230.kth-smallest-element-in-a-bst.171372802.Accepted.leetcode.java
@@ -5,13 +5,14 @@ public class Solution
 
     public int kthSmallest(TreeNode root, int k)
     {
+        count = 0;
         inorderTraversal(root, k);
         return result;
     }
 
     private void inorderTraversal(TreeNode root, int k)
     {
-        if (root == null || count > k)
+        if (root == null)
         {
             return;
         }
