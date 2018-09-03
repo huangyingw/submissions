d61ed24f563b4c88acf494d9ed2400f149c61faa
Ying Huang <yhuang@imvu.com>
2018-09-03 08:26:27 -0700

    n

---
+++ 206.reverse-linked-list.165690864.Runtime-Error.leetcode.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def reverseList(self, head):
+        def helper(head):
+            if not head or not head.next:
+                return head
+
+            second = head.next
+            ret = helper(second)
+            second.next = head
+            return ret
+
+        result = helper(head)
+        head.next = None
+        return result
