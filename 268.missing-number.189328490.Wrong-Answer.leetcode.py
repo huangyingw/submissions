22d170267f6309ab8909620d14492637477464e3
Ying Huang <yhuang@imvu.com>
2018-11-14 06:40:10 -0800

    n

---
+++ 268.missing-number.189328490.Wrong-Answer.leetcode.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        for num in nums:
+            if num < len(nums):
+                nums[num] = num
+
+        missing = len(nums)
+
+        for num in nums:
+            if num < len(nums) and nums[num] != num:
+                missing = num
+
+        return missing
