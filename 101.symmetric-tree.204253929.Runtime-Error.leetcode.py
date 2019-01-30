0ee9c4641e568a6cd931b39c294c81b2b662150b
Ying Huang <yhuang@imvu.com>
2019-01-30 15:46:17 -0800

    n

---
+++ 101.symmetric-tree.204253929.Runtime-Error.leetcode.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def helper(self, left, right):
+        if not left and not right:
+            return True
+
+        if not left or not right:
+            return False
+
+        return left.val == right.val and self.helper(left.left, right.right) and self.helper(left.right, right.left)
+
+    def isSymmetric(self, root):
+        return self.helper(root.left, root.right)
---
+++ 179.largest-number.164419693.Accepted.leetcode.java
@@ -0,0 +1,29 @@
+public class Solution
+{
+    public String largestNumber(int[] nums)
+    {
+        StringBuilder result = new StringBuilder();
+        List<Integer> datas = new ArrayList<Integer>();
+
+        for (int i : nums)
+        {
+            datas.add(i);
+        }
+
+        Collections.sort(datas, new Comparator<Integer>()
+        {
+            public int compare(Integer o1, Integer o2)
+            {
+                return (o2 + "" + o1).compareTo(o1 + "" + o2);
+            }
+        });
+
+        for (int nav = 0; nav < datas.size(); nav++)
+        {
+            result.append(datas.get(nav));
+        }
+
+        return result.toString().replaceFirst("^0+(?!$)", "");
+    }
+}
+
