41d13ef39a54a1a94efbbf5af2d5bb2ed0be5642
Ying Huang <yhuang@imvu.com>
2019-02-05 23:21:34 -0800

    n

---
+++ 35.search-insert-position.34754531.Accepted.leetcode.java
@@ -0,0 +1,29 @@
+  public class Solution
+  {
+    public int searchInsert(int[] A, int target)
+    {
+      int left = 0;
+      int right = A.length - 1;
+
+      while (left <= right)
+      {
+        int mid = (right + left) / 2;
+
+        if (A[mid] == target)
+        {
+          return mid;
+        }
+        else if (A[mid] < target)
+        {
+          left = mid + 1;
+        }
+        else
+        {
+          right = mid - 1;
+        }
+      }
+
+      return left;
+    }
+  }
+
