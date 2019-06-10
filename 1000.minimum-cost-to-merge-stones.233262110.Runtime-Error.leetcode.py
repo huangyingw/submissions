0d3c3d563bc6e98ed74f2d2471c5120585268955
Ying Huang <huangyingw@gmail.com>
2019-06-09 22:18:20 -0700

    n

--- 1000.minimum-cost-to-merge-stones.233262110.Runtime-Error.leetcode.py
+++ 1000.minimum-cost-to-merge-stones.233262110.Runtime-Error.leetcode.py
@@ -1,20 +1,3 @@
-_author_ = 'jake'
-_project_ = 'leetcode'
-
-# https://leetcode.com/problems/minimum-cost-to-merge-stones/
-# There are N piles of stones arranged in a row.  The i-th pile has stones[i] stones.
-# A move consists of merging exactly K consecutive piles into one pile, and the cost of this move is equal to the
-# total number of stones in these K piles.
-# Find the minimum cost to merge all piles of stones into one pile.  If it is impossible, return -1.
-
-# Return -1 if repeated merges will not leave one stone.
-# For a given subarray, try all possible splits into a prefix and a suffix, which are separately merged.
-# The prefix may be a single pile (which ensures that there is some value passed to the min function) or any prefixe of
-# a length that can be merged.
-# If the subarray is of the correct length to be merged, each pile is counted once.
-# Time - O(n**3)
-# Space - O(n**2)
-
 import functools
 
 
