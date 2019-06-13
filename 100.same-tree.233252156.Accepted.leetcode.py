6b39c3593dd1a39a912dc3ace1c1bec3e8348160
Ying Huang <huangyingw@gmail.com>
2019-06-12 22:32:24 -0700

    n

--- 100.same-tree.233252156.Accepted.leetcode.py
+++ 100.same-tree.233252156.Accepted.leetcode.py
@@ -1,34 +1,5 @@
-'''
-	Given two binary trees, write a function to check if they are the same or not.
-
-	Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
-
-	Example 1:
-
-	Input:     1         1
-	          / \       / \
-	         2   3     2   3
-
-	        [1,2,3],   [1,2,3]
-
-	Output: true
-'''
-
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
-
 class Solution(object):
     def isSameTree(self, p, q):
-        """
-        :type p: TreeNode
-        :type q: TreeNode
-        :rtype: bool
-        """
         if not p and not q:
             return True
 
--- 102.binary-tree-level-order-traversal.233262171.Accepted.leetcode.py
+++ 102.binary-tree-level-order-traversal.233262171.Accepted.leetcode.py
@@ -1,27 +1,5 @@
-_author_ = 'jake'
-_project_ = 'leetcode'
-
-# https://leetcode.com/problems/binary-tree-level-order-traversal/
-# Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
-
-# Create a list of all non-null nodes from the nodes in the level above.
-# Time - O(n)
-# Space - O(n)
-
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
-
 class Solution(object):
     def levelOrder(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[List[int]]
-        """
         result = []
         if not root:
             return result
