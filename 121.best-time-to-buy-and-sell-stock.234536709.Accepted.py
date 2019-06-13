ecce4a5df5fee2c30cf72fa321c4ecf410d16c4f
Ying Huang <huangyingw@gmail.com>
2019-06-12 22:54:01 -0700

    n

--- 121.best-time-to-buy-and-sell-stock.234536709.Accepted.py
+++ 121.best-time-to-buy-and-sell-stock.234536709.Accepted.py
@@ -1,14 +1,8 @@
-# https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
 import sys
 
 
 class Solution:
     def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
-        # 只做一次交易，需要保证找出最小值与最大值，且最小值在最大值之前。
         minPrice = sys.maxsize
         maxProfit = 0
         for p in prices:
--- 15.3sum.233031150.Accepted.leetcode.py
+++ 15.3sum.233031150.Accepted.leetcode.py
@@ -1,28 +1,5 @@
-'''
-	Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
-
-	Note:
-
-	The solution set must not contain duplicate triplets.
-
-	Example:
-
-	Given array nums = [-1, 0, 1, 2, -1, -4],
-
-	A solution set is:
-	[
-	  [-1, 0, 1],
-	  [-1, -1, 2]
-	]
-'''
-
-
 class Solution(object):
     def threeSum(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
         nums.sort()
 
         if (len(nums) >= 3) and (nums[0] == nums[len(nums) - 1]) and (nums[0] == 0):
@@ -44,6 +21,3 @@ class Solution(object):
                 else:
                     right -= 1
         return [list(t) for t in set(tuple(element) for element in result)]
-
-# Space: O(1)
-# Time: O(N^2)
