b41d3e6080eefb67f753e9753b559e336f9b8bac
Ying Huang <huangyingw@gmail.com>
2019-07-07 16:23:18 -0700

    n

---
+++ 141.linked-list-cycle.241353510.Runtime-Error.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def hasCycle(self, head):
+        if not head or not head.next:
+            return False
+        slow, fast = head.next, head.next.next
+        while slow != fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return True
