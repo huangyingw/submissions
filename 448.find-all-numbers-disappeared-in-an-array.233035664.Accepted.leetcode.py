418b97fa9c2b00d5723e56ddd6166251f0803bd1
Ying Huang <huangyingw@gmail.com>
2019-07-06 23:22:39 -0700

    n

--- 448.find-all-numbers-disappeared-in-an-array.233035664.Accepted.leetcode.py
+++ 448.find-all-numbers-disappeared-in-an-array.233035664.Accepted.leetcode.py
@@ -1,10 +1,5 @@
 class Solution(object):
     def findDisappearedNumbers(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        # https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/discuss/92956/Java-accepted-simple-solution
         res = []
         if nums:
             n = len(nums)
--- 448.find-all-numbers-disappeared-in-an-array.234403072.Accepted.py
+++ 448.find-all-numbers-disappeared-in-an-array.234403072.Accepted.py
@@ -1,12 +1,5 @@
-# https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/description/
 class Solution:
-        # 1
-        # 先使用set,然后遍历1-n,将不在set中的取出
     def findDisappearedNumbers(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
         length = len(nums)
         num_set = set(nums)
         res = []
@@ -14,8 +7,3 @@ class Solution:
             if i not in num_set:
                 res.append(i)
         return res
-    # 2 使用两个set相减，耗时长
-
-    def findDisappearedNumbers2(self, nums):
-        all_nums = [i for i in range(1, len(nums) + 1)]
-        return list(set(all_nums) - set(nums))
