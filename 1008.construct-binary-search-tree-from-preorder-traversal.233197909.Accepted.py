771cbce74bd1d51b41e95d968fcbf67545cc4f95
Ying Huang <huangyingw@gmail.com>
2019-07-08 23:29:12 -0700

    n

--- 1008.construct-binary-search-tree-from-preorder-traversal.233197909.Accepted.py
+++ 1008.construct-binary-search-tree-from-preorder-traversal.233197909.Accepted.py
@@ -1,7 +1,4 @@
-'''
-Return the root node of a binary search tree that matches the given preorder traversal.
-(Recall that a binary search tree is a binary tree where for every node, any descendant of node.left has a value < node.val, and any descendant of node.right has a value > node.val.  Also recall that a preorder traversal displays the value of the node first, then traverses node.left, then traverses node.right.)
-'''
+
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
--- 105.construct-binary-tree-from-preorder-and-inorder-traversal.233196777.Accepted.py
+++ 105.construct-binary-tree-from-preorder-and-inorder-traversal.233196777.Accepted.py
@@ -1,13 +1,4 @@
-'''
-	Given a non-empty string s and a dictionary wordDict containing a list of non-empty words, determine if s can be segmented into a space-separated sequence of one or more dictionary words.
-	Note:
-	The same word in the dictionary may be reused multiple times in the segmentation.
-	You may assume the dictionary does not contain duplicate words.
-	Example 1:
-	Input: s = "leetcode", wordDict = ["leet", "code"]
-	Output: true
-	Explanation: Return true because "leetcode" can be segmented as "leet code".
-'''
+
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
