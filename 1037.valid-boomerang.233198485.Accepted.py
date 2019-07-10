b90231727a557b6eaee791de520d57404c685b49
Ying Huang <huangyingw@gmail.com>
2019-07-10 02:36:51 -0400

    n

--- 1037.valid-boomerang.233198485.Accepted.py
+++
@@ -1,9 +0,0 @@
-
-
-
-class Solution(object):
-    def isBoomerang(self, points):
-        x1, x2, x3, y1, y2, y3 = points[0][0], points[1][0], points[2][0], points[0][1], points[1][1], points[2][1]
-        if ((y3 - y2) * (x2 - x1) == (y2 - y1) * (x3 - x2)):
-            return False
-        return True
--- 326.power-of-three.233199560.Accepted.py
+++
@@ -1,9 +0,0 @@
-
-
-
-class Solution(object):
-    def isPowerOfThree(self, n):
-        if n <= 0:
-            return False
-        import math
-        return (math.log10(n) / math.log10(3)) % 1 == 0
