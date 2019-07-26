ede2c681a1852b40fd590526dbc44246b32a2672
Ying Huang <huangyingw@gmail.com>
2019-07-25 23:22:34 -0700

    n

--- 100.same-tree.234345886.Accepted.py
+++ 100.same-tree.234345886.Accepted.py
@@ -1,10 +1,3 @@
-class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
-
-
 class Solution:
     def isSameTree(self, p, q):
         if not p or not q:
