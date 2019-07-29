a2c1f228c4dae9ab0382763268b4ceac4b3e750c
Ying Huang <huangyingw@gmail.com>
2019-07-28 23:26:08 -0700

    n

--- 125.valid-palindrome.233262780.Accepted.py
+++ 125.valid-palindrome.233262780.Accepted.py
@@ -5,10 +5,10 @@ class Solution(object):
     def isPalindrome(self, s):
         allowed = set(string.ascii_lowercase + string.digits)
         s = [c for c in s.lower() if c in allowed]
-        i, j = 0, len(s) - 1
-        while i < j:
-            if s[i] != s[j]:
+        start, end = 0, len(s) - 1
+        while start < end:
+            if s[start] != s[end]:
                 return False
-            i += 1
-            j -= 1
+            start += 1
+            end -= 1
         return True
