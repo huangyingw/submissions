0d6ea8d84981cdbedf368e83251245c3831d5e1c
Ying Huang <huangyingw@gmail.com>
2019-07-29 23:22:35 -0700

    n

--- 111.minimum-depth-of-binary-tree.157402335.Accepted.py
+++ 111.minimum-depth-of-binary-tree.157402335.Accepted.py
@@ -1,9 +1,5 @@
 class Solution(object):
     def minDepth(self, root):
-        """
-        :type root: TreeNode
-        :rtype: int
-        """
         if not root:
             return 0
         left = self.minDepth(root.left)
