7e215c78e5ea5dbb9e5a57b8ade10ad657627f29
Ying Huang <huangyingw@gmail.com>
2019-07-29 23:24:02 -0700

    n

--- 111.minimum-depth-of-binary-tree.234226273.Accepted.py
+++ 111.minimum-depth-of-binary-tree.234226273.Accepted.py
@@ -1,28 +1,5 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
-
 class Solution(object):
-    # def minDepth(self, root):
-    #     """
-    #     :type root: TreeNode
-    #     :rtype: int
-    #     """
-    #     # Recursion
-    #     if root is None:
-    #         return 0
-    #     ld = self.minDepth(root.left)
-    #     rd = self.minDepth(root.right)
-    #     if ld != 0 and rd != 0:
-    #         # handle 0 case!
-    #         return 1 + min(ld, rd)
-    #     return 1 + ld +rd
     def minDepth(self, root):
-        # BFS
         if root is None:
             return 0
         queue = [root]
@@ -36,7 +13,6 @@ class Solution(object):
             if node.right is not None:
                 queue.append(node.right)
             if node == rightMost:
-                # reach the current level end
                 depth += 1
                 if node.right is not None:
                     rightMost = node.right
