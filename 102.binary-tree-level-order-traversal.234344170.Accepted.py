5f32985d2bfde479e19da198678920f9078e4603
Ying Huang <huangyingw@gmail.com>
2019-08-07 22:19:36 +0800

    n

--- 102.binary-tree-level-order-traversal.234344170.Accepted.py
+++ 102.binary-tree-level-order-traversal.234344170.Accepted.py
@@ -1,10 +1,3 @@
-class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
-
-
 class Solution:
     def levelOrder(self, root):
         if not root:
@@ -12,6 +5,6 @@ class Solution:
         result = []
         this_level = [root]
         while this_level:
-            result.append([node.val for node in this_level])
+            result += [[node.val for node in this_level]]
             this_level = [child for node in this_level for child in (node.left, node.right) if child]
         return result
