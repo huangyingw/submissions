56fc8f7a0a18a68da1f4d52822f0d094c2960549
Ying Huang <huangyingw@gmail.com>
2019-08-07 22:13:44 +0800

    n

--- 102.binary-tree-level-order-traversal.233262171.Accepted.leetcode.py
+++ 102.binary-tree-level-order-traversal.233262171.Accepted.leetcode.py
@@ -6,12 +6,12 @@ class Solution(object):
         level_nodes = [root]
         while level_nodes:
             new_level_nodes = []
-            result.append([])
+            result += [[]]
             for node in level_nodes:
-                result[-1].append(node.val)
+                result[-1] += [node.val]
                 if node.left:
-                    new_level_nodes.append(node.left)
+                    new_level_nodes += [node.left]
                 if node.right:
-                    new_level_nodes.append(node.right)
+                    new_level_nodes += [node.right]
             level_nodes = new_level_nodes
         return result
