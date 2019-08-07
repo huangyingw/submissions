ff7b69954f74ae2114e3e8fe92eb1ff890c22ed5
Ying Huang <huangyingw@gmail.com>
2019-08-07 21:20:59 +0800

    n

--- 102.binary-tree-level-order-traversal.233032517.Accepted.leetcode.py
+++ 102.binary-tree-level-order-traversal.233032517.Accepted.leetcode.py
@@ -1,15 +1,15 @@
 class Solution(object):
     def levelOrder(self, root):
-        if root is None:
+        if not root:
             return []
         q = [[root]]
         for level in q:
             record = []
             for node in level:
                 if node.left:
-                    record.append(node.left)
+                    record += [node.left]
                 if node.right:
-                    record.append(node.right)
+                    record += [node.right]
             if record:
-                q.append(record)
+                q += [record]
         return [[x.val for x in level] for level in q]
