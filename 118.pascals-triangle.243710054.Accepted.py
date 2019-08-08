24af806e38e6136f07f396956723c3a4c99738d5
Ying Huang <huangyingw@gmail.com>
2019-08-08 20:42:10 +0800

    n

--- 118.pascals-triangle.243710054.Accepted.py
+++ 118.pascals-triangle.243710054.Accepted.py
@@ -6,6 +6,6 @@ class Solution(object):
         for i in range(1, numRows):
             result += [[1]]
             for num1, num2 in zip(result[-2][:-1], result[-2][1:]):
-                result[-1].append(num1 + num2)
+                result[-1] += [num1 + num2]
             result[-1] += [1]
         return result
