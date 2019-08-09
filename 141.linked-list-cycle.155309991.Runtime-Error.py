ca776906667ea853f1aa6a9afce8b170b84f9f51
Ying Huang <huangyingw@gmail.com>
2019-08-09 09:10:06 +0800

    n

--- 141.linked-list-cycle.155309991.Runtime-Error.py
+++ 141.linked-list-cycle.155309991.Runtime-Error.py
@@ -1,15 +1,5 @@
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
-
 class Solution(object):
     def hasCycle(self, head):
-        """
-        :type head: ListNode
-        :rtype: bool
-        """
         slow, fast = head.next
         while slow != fast:
             slow = slow.next
