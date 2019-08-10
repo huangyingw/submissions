43cfa84285e9a116c4ae116f294530dc4c3c665b
Ying Huang <huangyingw@gmail.com>
2019-08-10 22:56:25 +0800

    n

--- 153.find-minimum-in-rotated-sorted-array.234555790.Accepted.py
+++ 153.find-minimum-in-rotated-sorted-array.234555790.Accepted.py
@@ -4,15 +4,15 @@ class Solution(object):
             return nums[0]
         if nums[0] < nums[-1]:
             return nums[0]
-        start = 0
-        end = len(nums) - 1
-        while start < end:
-            mid = (start + end) // 2
+        left = 0
+        right = len(nums) - 1
+        while left < right:
+            mid = (left + right) // 2
             if nums[mid] > nums[mid + 1]:
                 return nums[mid + 1]
             if nums[mid - 1] > nums[mid]:
                 return nums[mid]
-            elif nums[mid] > nums[end]:
-                start = mid + 1
+            elif nums[mid] > nums[right]:
+                left = mid + 1
             else:
-                end = mid + 1
+                right = mid + 1
