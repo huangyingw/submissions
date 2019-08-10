ca40fed8fd34aca6398b6d51e0ba3398ce0e1891
Ying Huang <huangyingw@gmail.com>
2019-08-10 22:58:11 +0800

    n

--- 153.find-minimum-in-rotated-sorted-array.210489085.Accepted.leetcode.py
+++ 153.find-minimum-in-rotated-sorted-array.210489085.Accepted.leetcode.py
@@ -1,9 +1,6 @@
 class Solution(object):
     def findMin(self, nums):
-        if not nums:
-            return 0
-        left = 0
-        right = len(nums) - 1
+        left, right = 0, len(nums) - 1
         while left + 1 < right:
             mid = (left + right) / 2
             if nums[mid] < nums[right]:
