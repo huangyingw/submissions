1e6d2236b68d49793f4228df099773f9584797f3
Ying Huang <huangyingw@gmail.com>
2019-08-13 15:45:11 +0800

    n

--- 209.minimum-size-subarray-sum.234348721.Accepted.py
+++ 209.minimum-size-subarray-sum.234348721.Accepted.py
@@ -1,12 +1,12 @@
 class Solution:
     def minSubArrayLen(self, s, nums):
-        l, r = 0, 0
+        left, right = 0, 0
         ret, sum = float('inf'), 0
-        while r < len(nums):
-            sum += nums[r]
-            r += 1
+        while right < len(nums):
+            sum += nums[right]
+            right += 1
             while sum >= s:
-                ret = min(ret, r - l)
-                sum -= nums[l]
-                l += 1
+                ret = min(ret, right - left)
+                sum -= nums[left]
+                left += 1
         return 0 if ret == float('inf') else ret
