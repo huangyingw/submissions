08f0e223a495ae7ae6e863380c8a483ed7820bf9
Ying Huang <huangyingw@gmail.com>
2019-08-13 18:23:08 +0800

    n

--- 21.merge-two-sorted-lists.233031120.Accepted.leetcode.py
+++ 21.merge-two-sorted-lists.233031120.Accepted.leetcode.py
@@ -1,7 +1,7 @@
 class Solution(object):
     def mergeTwoLists(self, l1, l2):
-        pos = dummyHead = ListNode(-1)
-        while l1 is not None and l2 is not None:
+        pos = dummy = ListNode(-1)
+        while l1 and l2:
             if l1.val <= l2.val:
                 pos.next = l1
                 l1 = l1.next
@@ -9,8 +9,8 @@ class Solution(object):
                 pos.next = l2
                 l2 = l2.next
             pos = pos.next
-        if l1 is not None:
+        if l1:
             pos.next = l1
-        if l2 is not None:
+        if l2:
             pos.next = l2
-        return dummyHead.next
+        return dummy.next
