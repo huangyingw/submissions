9d734da260e63151dd79b48dea4f314b7e2e816f
Ying Huang <huangyingw@gmail.com>
2019-08-13 18:24:00 +0800

    n

--- 21.merge-two-sorted-lists.233259921.Accepted.py
+++ 21.merge-two-sorted-lists.233259921.Accepted.py
@@ -1,9 +1,3 @@
-class ListNode(object):
-    def __init__(self, x):
-        self.val = x
-        self.next = None
-
-
 class Solution(object):
     def mergeTwoLists(self, l1, l2):
         prev = dummy = ListNode(None)
