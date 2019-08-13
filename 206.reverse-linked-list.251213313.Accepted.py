19295c14aad6842aefc12b078d69a9f7609973dd
Ying Huang <huangyingw@gmail.com>
2019-08-13 06:21:33 -0700

    n

---
+++ 206.reverse-linked-list.251213313.Accepted.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def reverseList(self, head):
+        prev = None
+        while head:
+            temp = head.next
+            head.next = prev
+            prev = head
+            head = temp
+        return prev
