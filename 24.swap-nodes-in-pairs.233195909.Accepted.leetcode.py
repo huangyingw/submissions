7b9fc204ecb44825c37bd8b2395ea56eec31607c
Ying Huang <huangyingw@gmail.com>
2019-08-15 11:35:34 +0800

    n

--- 24.swap-nodes-in-pairs.233195909.Accepted.leetcode.py
+++ 24.swap-nodes-in-pairs.233195909.Accepted.leetcode.py
@@ -1,9 +1,9 @@
 class Solution(object):
     def swapPairs(self, head):
-        if head is None:
+        if not head:
             return head
         ref = head
-        while ref is not None and ref.next is not None:
+        while ref and ref.next:
             ref.val, ref.next.val = ref.next.val, ref.val
             ref = ref.next.next
         return head
