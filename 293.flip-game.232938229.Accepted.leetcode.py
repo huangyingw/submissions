20ca1ea88f98e7a87da7ecbb3e2e1ac8e20c5bc5
Ying Huang <huangyingw@gmail.com>
2019-08-17 11:01:03 +0800

    n

--- 293.flip-game.232938229.Accepted.leetcode.py
+++ 293.flip-game.232938229.Accepted.leetcode.py
@@ -1,3 +1,3 @@
 class Solution(object):
     def generatePossibleNextMoves(self, s):
-        return [(s[:i] + "--" + s[i + 2:]) for i in xrange(len(s)) if s[i:i + 2] == "++"]
+        return [(s[:i] + "--" + s[i + 2:]) for i in range(len(s)) if s[i:i + 2] == "++"]
