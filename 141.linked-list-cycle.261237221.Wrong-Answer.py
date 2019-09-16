3957dfaf0c3febefc215621b54cf87c2b03549ba
Ying Huang <huangyingw@gmail.com>
2019-09-16 09:19:43 -0700

    n

---
+++ 141.linked-list-cycle.261237221.Wrong-Answer.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def hasCycle(self, head):
+        try:
+            fast = head.next.next
+            slow = head.next
+            while fast != slow:
+                fast = fast.next.next
+                slow = slow.next
+        except:
+            return False
