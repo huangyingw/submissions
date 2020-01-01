7061421def0c65839e11b4568b6479549946f716
Ying Huang <huangyingw@gmail.com>
2019-12-31 21:15:13 -0800

    n

---
+++ 58.length-of-last-word.289952796.Accepted.leetcode.python3.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        temp = s.split(' ')
+        temp = [t for t in temp if len(t) > 0]
+        if len(temp) == 0:
+            return 0
+        else:
+            return len(temp[-1])
