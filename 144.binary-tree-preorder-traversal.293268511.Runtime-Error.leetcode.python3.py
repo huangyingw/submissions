e0fe16fae9a7bfaf325dddb4ea61d5a8220ac24f
Ying Huang <huangyingw@gmail.com>
2020-01-12 16:07:00 -0800

    n

---
+++ 144.binary-tree-preorder-traversal.293268511.Runtime-Error.leetcode.python3.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def preorderTraversal(self, root):
+        preorder = []
+        stack = []
+        while stack or root:
+            node = stack.pop()
+            preorder.append(node.val)
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
+        return preorder
