f14f66214445d7e9f3c9992e06bc6b3187ae47ea
Ying Huang <huangyingw@gmail.com>
2020-01-15 19:41:20 -0800

    n

--- 792.number-of-matching-subsequences.233986870.Accepted.leetcode.py
+++ 792.number-of-matching-subsequences.233986870.Accepted.leetcode.py
@@ -5,14 +5,14 @@ class Solution(object):
     def numMatchingSubseq(self, S, words):
         letter_to_suffixes = defaultdict(list)
         letter_to_suffixes["
-                           result = 0
+                           result= 0
                            for c in "
-                           suffixes = letter_to_suffixes[c]
+                           suffixes= letter_to_suffixes[c]
                            del letter_to_suffixes[c]
                            for suffix in suffixes:
                            if len(suffix) == 0:
                            result += 1
                            continue
-                           next_letter, next_suffix = suffix[0], suffix[1:]
+                           next_letter, next_suffix= suffix[0], suffix[1:]
                            letter_to_suffixes[next_letter].append(next_suffix)
                            return result
