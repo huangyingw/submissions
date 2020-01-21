2aee94ed6f8c01afed4cd9147d7d90412a24762c
Ying Huang <huangyingw@gmail.com>
2020-01-20 19:55:15 -0800

    n

--- 138.copy-list-with-random-pointer.234547993.Runtime-Error.leetcode.py
+++ 138.copy-list-with-random-pointer.234547993.Runtime-Error.leetcode.py
@@ -1,9 +1,3 @@
-"""
-A linked list is given such that each node contains an additional random pointer which could point to any node in the list or null.
-Return a deep copy of the list.
-"""
-
-
 class Solution(object):
     def copyRandomList(self, head):
         if not head:
--- 965.univalued-binary-tree.234543170.Accepted.leetcode.py
+++ 965.univalued-binary-tree.234543170.Accepted.leetcode.py
@@ -1,15 +1,3 @@
-"""
-A binary tree is univalued if every node in the tree has the same value.
-Return true if and only if the given tree is univalued.
-Example 1:
-Input: [1,1,1,1,1,null,1]
-Output: true
-Example 2:
-Input: [2,2,2,5,2]
-Output: false
-"""
-
-
 class Solution:
     def isUnivalTree(self, root):
         vals = []
