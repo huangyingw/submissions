f0b14e3565076eddc08b7fa2705dff2afb373f46
Ying Huang <huangyingw@gmail.com>
2020-01-22 09:55:17 -0800

    n

---
+++ 387.first-unique-character-in-a-string.296218966.Accepted.leetcode.python3.py
@@ -0,0 +1,9 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        alphabets = {}
+        for c in s:
+            alphabets[c] = alphabets.get(c, 0) + 1
+        for i in range(len(s)):
+            if alphabets.get(s[i]) == 1:
+                return i
+        return -1
