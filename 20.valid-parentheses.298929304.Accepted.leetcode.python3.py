fc33dee11edce3d9d830266cb4445b6858c8811c
Ying Huang <huangyingw@gmail.com>
2020-01-31 14:45:52 -0800

    n

---
+++ 20.valid-parentheses.298929304.Accepted.leetcode.python3.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValid(self, s: str):
+        stack = []
+        d = {
+            ')': '(',
+            '}': '{',
+            ']': '['
+        }
+        for c in s:
+            if c in d.values():
+                stack.append(c)
+            elif (not stack) or (stack.pop() != d[c]):
+                return False
+        return not stack
