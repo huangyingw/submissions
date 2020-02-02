6bdbe9cd2db66e13f51e322e1f3726c5fa9704ba
Ying Huang <huangyingw@gmail.com>
2020-02-01 21:40:44 -0800

    n

---
+++ 129.sum-root-to-leaf-numbers.299389122.Runtime-Error.leetcode.python3.py
@@ -0,0 +1,7 @@
+class Solution:
+    def sumNumbers(self, root, summ=0):
+        if not root:
+            return 0
+        if not root.left and not root.right:
+            return summ * 10 + root.val
+        return self.getSum(root.left, summ * 10 + root.val) + self.getSum(root.right, summ * 10 + root.val)
---
+++ 1290.convert-binary-number-in-a-linked-list-to-integer.298914781.Accepted.leetcode.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def getDecimalValue(self, head):
+        result = ''
+        if not head:
+            return 0
+        while head:
+            result += str(head.val)
+            head = head.next
+        return int(result, 2)
