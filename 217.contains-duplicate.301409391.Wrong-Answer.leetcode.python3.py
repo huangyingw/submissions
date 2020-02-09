e1e8182837360a8abd2ed57765686e72b6ea3b4b
Ying Huang <huangyingw@gmail.com>
2020-02-08 21:16:39 -0800

    n

---
+++ 217.contains-duplicate.301409391.Wrong-Answer.leetcode.python3.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        s = set()
+        for num in nums:
+            if num not in s:
+                s.add(num)
+            return True
+        return False
---
+++ 217.contains-duplicate.301409534.Accepted.leetcode.python3.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        s = set()
+        for num in nums:
+            if num in s:
+                return True
+            s.add(num)
+        return False
