be983e74aa3970bc35367fc5239e2b90dc44e1dc
Ying Huang <huangyingw@gmail.com>
2020-02-11 09:19:29 -0800

    n

---
+++ 485.max-consecutive-ones.301899574.Accepted.leetcode.python3.py
@@ -0,0 +1,4 @@
+class Solution:
+    def findMaxConsecutiveOnes(self, nums):
+        temp = bytearray(nums).split(b'\x00')
+        return max([len(i) for i in temp])
---
+++ 495.teemo-attacking.301934733.Accepted.leetcode.python3.py
@@ -0,0 +1,5 @@
+class Solution:
+    def findPoisonedDuration(self, timeSeries, duration):
+        if len(timeSeries) == 0:
+            return 0
+        return sum(min(duration, i - j) for i, j in zip(timeSeries[1:], timeSeries[:-1])) + duration
