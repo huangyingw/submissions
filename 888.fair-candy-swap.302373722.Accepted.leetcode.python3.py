28f25f0ba90f94def7fcfc1ce0b64f573ab93c08
Ying Huang <huangyingw@gmail.com>
2020-02-12 21:38:50 -0800

    n

---
+++ 888.fair-candy-swap.302373722.Accepted.leetcode.python3.py
@@ -0,0 +1,7 @@
+class Solution:
+    def fairCandySwap(self, A, B):
+        sb = set(B)
+        diff = (sum(B) - sum(A)) // 2
+        for num in A:
+            if num + diff in sb:
+                return num, num + diff
