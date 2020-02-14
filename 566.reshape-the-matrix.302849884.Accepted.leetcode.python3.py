5ed2f28a4dfad121053c053f6a9db59ea31e983a
Ying Huang <huangyingw@gmail.com>
2020-02-13 21:29:09 -0800

    n

---
+++ 566.reshape-the-matrix.302849884.Accepted.leetcode.python3.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def matrixReshape(self, nums, r, c):
+        flat = sum(nums, [])
+        if len(flat) != r * c:
+            return nums
+        tuples = zip(*([iter(flat)] * c))
+        return map(list, tuples)
---
+++ 589.n-ary-tree-preorder-traversal.302846869.Accepted.leetcode.python3.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def preorder(self, root):
+        if root == None:
+            return []
+        lst = [root.val]
+        for node in root.children:
+            lst += self.preorder(node)
+        return lst
