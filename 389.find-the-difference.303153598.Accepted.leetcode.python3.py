14a72670fab3a8ead06506c48fdd94d4d246d2e0
Ying Huang <huangyingw@gmail.com>
2020-02-15 15:32:54 -0800

    n

---
+++ 389.find-the-difference.303153598.Accepted.leetcode.python3.py
@@ -0,0 +1,5 @@
+class Solution:
+    def findTheDifference(self, s, t):
+        for c in 'abcdefghijklmnopqrstuvwxyz':
+            if s.count(c) != t.count(c):
+                return c
---
+++ 48.rotate-image.234402662.Accepted.leetcode.py
@@ -0,0 +1,22 @@
+"""
+ * 1 2 3     7 8 9     7 4 1
+ * 4 5 6  => 4 5 6  => 8 5 2
+ * 7 8 9     1 2 3     9 6 3
+"""
+
+
+class Solution:
+    def rotate(self, matrix):
+        matrix[::] = list(zip(*matrix[::-1]))
+
+    def rotate(self, matrix):
+        matrix.reverse()
+        for i in range(len(matrix)):
+            for j in range(i):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    a = [
+        [15, 13, 2, 5],
+          [14, 3, 4, 1],
+          [12, 6, 8, 9],
+          [16, 7, 10, 11]
+    ]
