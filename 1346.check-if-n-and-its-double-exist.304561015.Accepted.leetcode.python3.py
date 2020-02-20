c913985194d78a21458b19a8f3e1f33185daf775
Ying Huang <huangyingw@gmail.com>
2020-02-19 20:22:31 -0800

    n

---
+++ 1346.check-if-n-and-its-double-exist.304561015.Accepted.leetcode.python3.py
@@ -0,0 +1,8 @@
+class Solution:
+    def checkIfExist(self, arr: List[int]) -> bool:
+        visited = set()
+        for no in arr:
+            if no // 2 in visited or no + no in visited:
+                return True
+            visited.add(no)
+        return False
