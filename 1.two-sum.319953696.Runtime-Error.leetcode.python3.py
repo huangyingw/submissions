65d4f2894eaa7bd5c8727511a548b6fb93043848
Ying Huang <huangyingw@gmail.com>
2020-05-01 10:16:25 -0700

    n

---
+++ 1.two-sum.319953696.Runtime-Error.leetcode.python3.py
@@ -0,0 +1,6 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        rest = 0
+        for num in nums:
+            rest ^= num
+        return rest
---
+++ 1221.split-a-string-in-balanced-strings.332533451.Accepted.leetcode.python3.py
@@ -0,0 +1,7 @@
+class Solution:
+    def balancedStringSplit(self, s: str) -> int:
+        count = res = 0
+        for c in s:
+            count += 1 if c == 'L' else -1
+            res += count == 0
+        return res
