399b2753fb5774a583c14f75e8c0c538a1ab7cfa
Ying Huang <huangyingw@gmail.com>
2020-05-03 17:22:31 -0700

    n

---
+++ 144.binary-tree-preorder-traversal.333960366.Wrong-Answer.leetcode.python3.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def preorderTraversal(self, root):
+        if not root:
+            return []
+        preorder = []
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            preorder.append(node.val)
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+        return preorder
