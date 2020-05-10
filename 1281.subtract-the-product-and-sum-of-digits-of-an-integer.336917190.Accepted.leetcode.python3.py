f7595de6eb10507cf78bbfa58ddef9d312f80b6c
Ying Huang <huangyingw@gmail.com>
2020-05-09 19:47:43 -0700

    n

---
+++ 1281.subtract-the-product-and-sum-of-digits-of-an-integer.336917190.Accepted.leetcode.python3.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def subtractProductAndSum(self, n):
+        product, total = 1, 0
+        while n:
+            n, digit = divmod(n, 10)
+            product *= digit
+            total += digit
+        return product - total
---
+++ 559.maximum-depth-of-n-ary-tree.336059556.Runtime-Error.leetcode.python3.py
@@ -0,0 +1,5 @@
+class Solution(object):
+    def maxDepth(self, root):
+        if not root.children:
+            return 1
+        return 1 + max(self.maxDepth(child) for child in root.children)
