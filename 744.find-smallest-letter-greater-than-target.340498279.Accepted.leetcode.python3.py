00d047dbf186e4963065c9eba09317dd3550c25e
Ying Huang <huangyingw@gmail.com>
2020-05-17 13:34:38 -0700

    n

---
+++ 744.find-smallest-letter-greater-than-target.340498279.Accepted.leetcode.python3.py
@@ -0,0 +1,12 @@
+class Solution:
+    def nextGreatestLetter(self, letters, target):
+        if ord(target) >= ord(letters[-1]):
+            return letters[0]
+        start, last = 0, len(letters)
+        while start < last:
+            mid = (start + last) // 2
+            if letters[mid] > target:
+                last = mid
+            else:
+                start = mid + 1
+        return letters[start]
