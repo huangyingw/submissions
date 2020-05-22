45a58b3965954d17fc7d9cf342e49427b8eb1927
Ying Huang <huangyingw@gmail.com>
2020-05-22 09:59:23 -0700

    n

---
+++ 690.employee-importance.342740766.Accepted.leetcode.python3.py
@@ -0,0 +1,9 @@
+class Solution:
+    def getImportance(self, employees: List['Employee'], id: int) -> int:
+        emap = {e.id: e for e in employees}
+
+        def dfs(eid):
+            employee = emap.get(eid)
+            return (employee.importance
+                    + sum(dfs(eid) for eid in employee.subordinates))
+        return dfs(id)
