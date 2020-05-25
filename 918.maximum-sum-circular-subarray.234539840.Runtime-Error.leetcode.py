5c3a62b08ec4b99a055d0b01fc0d5f1f5c524643
Ying Huang <huangyingw@gmail.com>
2020-05-25 16:55:05 -0700

    n

--- 918.maximum-sum-circular-subarray.234539840.Runtime-Error.leetcode.py
+++
@@ -1,9 +0,0 @@
-def maxSubarraySumCircular(self, A):
-    total, maxSum, curMax, minSum, curMin = 0, -float('inf'), 0, float('inf'), 0
-    for a in A:
-        curMax = max(curMax + a, a)
-        maxSum = max(maxSum, curMax)
-        curMin = min(curMin + a, a)
-        minSum = min(minSum, curMin)
-        total += a
-    return max(maxSum, total - minSum) if maxSum > 0 else maxSum
