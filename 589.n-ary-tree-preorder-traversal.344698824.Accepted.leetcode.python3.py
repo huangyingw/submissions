4665d87bdd20bd91beb993e273190f34b4a7f42e
Ying Huang <huangyingw@gmail.com>
2020-05-25 19:31:10 -0700

    n

---
+++ 589.n-ary-tree-preorder-traversal.344698824.Accepted.leetcode.python3.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def preorder(self, root):
+        if not root:
+            return []
+        lst = [root.val]
+        for node in root.children:
+            lst += self.preorder(node)
+        return lst
---
+++ 589.n-ary-tree-preorder-traversal.344699890.Runtime-Error.leetcode.python3.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def preorder(self, root):
+        result = []
+        stack = [root]
+        while stack:
+            cur = stack.pop()
+            result.append(cur.val)
+            stack.extend(reversed(cur.children))
+        return result
