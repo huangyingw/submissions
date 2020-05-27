31df9ed729b39a733f5ddcc0ce2f4c7f3105a096
Ying Huang <huangyingw@gmail.com>
2020-05-26 17:31:04 -0700

    n

--- 594.longest-harmonious-subsequence.234548329.Accepted.leetcode.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def findLHS(self, nums):
-        from collections import Counter
-        res = 0
-        mapping = Counter(nums)
-        for k in mapping.keys():
-            if k + 1 in mapping:
-                res = max(res, mapping.get(k) + mapping.get(k + 1))
-        return res
--- 771.jewels-and-stones.234548255.Accepted.leetcode.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def numJewelsInStones(self, J, S):
-        ret = 0
-        for i in J:
-            ret += S.count(i)
-        return ret
