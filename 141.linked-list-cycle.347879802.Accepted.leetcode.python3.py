9ffd503c44ae862f0beb932cf09fdea6b5d8f095
Ying Huang <huangyingw@gmail.com>
2020-06-02 09:51:20 -0700

    n

---
+++ 141.linked-list-cycle.347879802.Accepted.leetcode.python3.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def hasCycle(self, head):
+        fast, slow = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if fast == slow:
+                return True
+        return False
