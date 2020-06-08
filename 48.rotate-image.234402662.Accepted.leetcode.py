71f2d1d404c9e9faa591339835292865d718a6c1
Ying Huang <huangyingw@gmail.com>
2020-06-08 16:38:32 -0700

    n

---
+++ 48.rotate-image.234402662.Accepted.leetcode.py
@@ -0,0 +1,15 @@
+class Solution:
+    def rotate(self, matrix):
+        matrix[::] = list(zip(*matrix[::-1]))
+
+    def rotate(self, matrix):
+        matrix.reverse()
+        for i in range(len(matrix)):
+            for j in range(i):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    a = [
+        [15, 13, 2, 5],
+          [14, 3, 4, 1],
+          [12, 6, 8, 9],
+          [16, 7, 10, 11]
+    ]
