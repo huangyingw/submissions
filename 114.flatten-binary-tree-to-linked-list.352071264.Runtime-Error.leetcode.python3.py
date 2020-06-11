f6a3f5bb2cf89f2761e12dc41b3150ec37633a5f
Ying Huang <huangyingw@gmail.com>
2020-06-11 11:13:58 -0700

    n

---
+++ 114.flatten-binary-tree-to-linked-list.352071264.Runtime-Error.leetcode.python3.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def flatten(self, root):
+        current = root
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
+            if current != node:
+                current.right = node
+                current.left = None
+                current = node
