63e8db08b03d55789b0b0d5e949d9f07773fe964
Ying Huang <huangyingw@gmail.com>
2021-07-04 12:19:00 -0700

    n

---
+++ 1/1.two-sum.496093729.Wrong-Answer.leetcode.python3.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        num_to_index = {}
+        for i, num in enumerate(nums):
+            num_to_index[num] = i
+            if target - num in num_to_index and target - num != num:
+                return [num_to_index[target - num], i]
