efc9eff002241dc7b2294e3ebb44e8d2aa739e2e
Ying Huang <huangyingw@gmail.com>
2022-01-08 21:50:06 -0800

    n

---
+++ 113/113.path-sum-ii.616038731.Wrong-Answer.leetcode.python3.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def pathSum(self, root, sum):
+        paths = []
+        self.preorder(root, sum, [], paths)
+        return paths
+
+    def preorder(self, node, target, partial, paths):
+        if not node:
+            return
+        if not node.left and not node.right and node.val == target:
+            paths.append(partial)
+        target -= node.val
+        partial.append(node.val)
+        self.preorder(node.left, target, partial, paths)
+        self.preorder(node.right, target, partial, paths)
+        partial.pop()
+        target += node.val
