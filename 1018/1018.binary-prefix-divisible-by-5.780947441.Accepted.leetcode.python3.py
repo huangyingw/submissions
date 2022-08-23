f31fab1892fbba60fde263fafee937c9a8df2282
Ying Huang <huangyingw@gmail.com>
2022-08-22 22:23:56 -0700

    n

---
+++ 1018/1018.binary-prefix-divisible-by-5.780947441.Accepted.leetcode.python3.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def prefixesDivBy5(self, A):
+        result = []
+        num = 0
+        for bit in A:
+            num = (num * 2 + bit) % 5
+            result.append(num == 0)
+        return result
