4e93a6a61333d241ff27faf49189cc83104a2e32
Ying Huang <huangyingw@gmail.com>
2022-09-30 18:07:11 -0700

    n

---
+++ 1695/1695.maximum-erasure-value.812371418.Wrong-Answer.leetcode.python3.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maximumUniqueSubarray(self, nums: List[int]) -> int:
+        last_seen = {}
+        start = 0
+        vmax = 0
+        for i, c in enumerate(nums):
+            if c in last_seen:
+                old_start = start
+                start = max(start, last_seen[c] + 1)
+                for idx in range(old_start, start):
+                    vmax -= nums[idx]
+                vmax = nums[start]
+            vmax += c
+            last_seen[c] = i
+        return vmax
